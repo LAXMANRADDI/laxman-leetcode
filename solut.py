fc45b65b203f682f592b756f3b489b54c27cafbb
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-15 22:41:25 +0530

    Update solut.py

--- solut.py
+++ solut.py
@@ -140,7 +140,7 @@ class Solution:
 input ::
 [3,2,2,3]
 3
-
 output ::
 [2,2]
 
+7 ]] Given two strings haystack and needle, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
