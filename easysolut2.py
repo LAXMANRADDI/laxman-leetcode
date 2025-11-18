8391dcb51c61c5723e564fd7f1846367a2346780
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-18 22:46:33 +0530

    Update easysolut2.py

--- easysolut2.py
+++ easysolut2.py
@@ -2,6 +2,9 @@
 return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 You must write an algorithm with O(log n) runtime complexity.
 
+approsch :
+* ]Use Binary Search to find the target efficiently in O(log n).
+* ]If target is not found, the final low pointer will give you the insert position. 
 
 def searchInsert(nums, target):
     low, high = 0, len(nums) - 1
@@ -15,5 +18,10 @@ def searchInsert(nums, target):
             low = mid + 1
         else:
             high = mid - 1
-            
+Input: nums = [1,3,5,6], target = 5
+Output: 2 # Target 5 is at index 2.
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1 #If 2 is inserted between 1 and 3, its index would be 1.
+
     return low  # insert position
